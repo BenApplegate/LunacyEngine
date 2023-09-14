7e269bbd7b4891c15529c9d48013dd7f90174dcd
BenApplegate <applegateben03@gmail.com>
2023-09-14 02:26:27 -0600

    Add OpenTK library with basic window creation

---
+++ LunacyEngine/Core/LunacyEngine.cs
@@ -0,0 +1,19 @@
+﻿using OpenTK.Windowing.Desktop;
+
+namespace LunacyEngine.Core;
+
+public static class LunacyEngine
+{
+    internal static GameWindow _window;
+    
+    public static void Initialize(int width = 800, int height = 600, string windowTitle = "Lunacy Game")
+    {
+        var windowSettings = new NativeWindowSettings() { Size = (width, height), Title = windowTitle};
+        _window = new GameWindow(GameWindowSettings.Default, windowSettings);
+    }
+
+    public static void Dispose()
+    {
+        _window.Dispose();
+    }
+}
