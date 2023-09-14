2d6feda0034f38cc6b732fa7357024e8a0957fb6
BenApplegate <applegateben03@gmail.com>
2023-09-14 02:57:28 -0600

    Added window creation with internal event loop

--- Lunacy/Core/LunacyEngine.cs
+++ Lunacy/Core/LunacyEngine.cs
@@ -1,6 +1,6 @@
 ﻿using OpenTK.Windowing.Desktop;
 
-namespace LunacyEngine.Core;
+namespace Lunacy.Core;
 
 public static class LunacyEngine
 {
@@ -8,8 +8,22 @@ public static class LunacyEngine
     
     public static void Initialize(int width = 800, int height = 600, string windowTitle = "Lunacy Game")
     {
+        //Create window settings from passed parameters and create window
         var windowSettings = new NativeWindowSettings() { Size = (width, height), Title = windowTitle};
         _window = new GameWindow(GameWindowSettings.Default, windowSettings);
+        
+        //Setup callback for window close event
+        bool windowShouldClose = false;
+        _window.Closing += args =>
+        {
+            windowShouldClose = true;
+        };
+        
+        //Event Loop
+        while (!windowShouldClose)
+        {
+            _window.ProcessEvents(1);
+        }
     }
 
     public static void Dispose()
---
+++ SampleGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using Lunacy.Core;
+
+namespace SampleGame;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        LunacyEngine.Initialize();
+        
+        
+        
+        LunacyEngine.Dispose();
+    }
+}
