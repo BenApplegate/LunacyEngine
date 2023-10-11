fca2568f68f8da3c20f345b4836ae6bb5a2fec04
BenApplegate <applegateben03@gmail.com>
2023-10-10 23:02:16 -0600

    Added samples for update loop

--- SampleGame/Program.cs
+++ SampleGame/Program.cs
@@ -6,8 +6,16 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        LunacyEngine.Initialize();
+        Scene scene = new Scene();
+        for (int i = 0; i < 50; i++)
+        {
+            GameObject testObject = new GameObject(scene, $"test_object_{i}");
+            testObject.AddComponent(new TestComponent());
+        }
         
+        
+        LunacyEngine.Initialize(scene);
+        LunacyEngine.Run();
         
         
         LunacyEngine.Dispose();
